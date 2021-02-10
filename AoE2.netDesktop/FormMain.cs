67cfbfd26d1d4b0213eaaffa58c731a93fc6a580
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2021-02-10 23:06:11 +0900

    Add solution

---
+++ AoE2.netDesktop/FormMain.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace AoE2.netDesktop
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
