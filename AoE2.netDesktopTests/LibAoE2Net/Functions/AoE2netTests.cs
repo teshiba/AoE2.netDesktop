f57ff42815a1233a8222b018041210771265fffd
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2021-02-15 22:42:56 +0900

    add AoE2net test

---
+++ AoE2.netDesktopTests/LibAoE2Net/Functions/AoE2netTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LibAoE2net;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibAoE2net.Tests
+{
+    [TestClass()]
+    public class AoE2netTests
+    {
+        [TestMethod()]
+        public void GetPlayerLastMatchAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPlayerRatingHistoryAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPlayerRatingHistoryAsyncTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetStringsAsyncTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetCivImageLocationTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
