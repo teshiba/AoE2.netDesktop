67c7735e78c407563e45bfc8df67521efeb8f92a
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2021-03-13 21:18:01 +0900

    add files to exclude in the coverage report

--- coverage.bat
+++ coverage.bat
@@ -13,22 +13,29 @@ rem target informations
 
 rem output ReportGenerator result directory
 set OUTPUT_DIR="Coverage"
+set HISTORY_DIR="Coverage/History"
+set REPORT_FILE_FILTERS="-*.Designer.cs;-*Program.cs"
 
-rem target filter
-set FILTERS="TestCategory!=GUI"
+rem test target filter
+set TEST_FILTERS="TestCategory!=GUI"
 
 rem ===========================================================================
 rem Run test
+echo on
+dotnet test --collect:"XPlat Code Coverage" --filter %TEST_FILTERS% > test.log
+echo off
 
-dotnet test --collect:"XPlat Code Coverage" --filter %FILTERS% | find "coverage.cobertura.xml" > coverageCoberturaPath.txt
+type test.log 
 
 rem Set COVERAGE_RESULT variable from Output of the dotnet test.
+type test.log | find "coverage.cobertura.xml" > coverageCoberturaPath.txt
 for /f %%a in (coverageCoberturaPath.txt) do @set COVERAGE_RESULT=%%a&goto :exit_for
 :exit_for
 del coverageCoberturaPath.txt
 
 rem ===========================================================================
 rem Report test result
+echo on
 
-%REPORTGEN% -reports:%COVERAGE_RESULT% -targetdir:%OUTPUT_DIR%
+%REPORTGEN% -filefilters:%REPORT_FILE_FILTERS% -targetdir:%OUTPUT_DIR% -historydir:%HISTORY_DIR% -reports:%COVERAGE_RESULT%
 %OUTPUT_DIR%\index.htm
