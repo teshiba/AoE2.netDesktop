77521cb5158ff24b00c987f6b4bd88dc97d0229e
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2022-07-16 16:27:16 +0900

    add ToString for debug

---
+++ AoE2.netDesktopTests/LibAoE2Net/JsonFormat/PlayerRatingTests.cs
@@ -0,0 +1,36 @@
+﻿namespace AoE2NetDesktop.LibAoE2Net.JsonFormat.Tests
+{
+    using AoE2NetDesktop.LibAoE2Net.JsonFormat;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class PlayerRatingTests
+    {
+        [TestMethod]
+        public void ToStringTest()
+        {
+            // Arrange
+            var expDrops = 1;
+            var expNumLosses = 2;
+            var expNumWins = 3;
+            var expRating = 1234;
+            var expStreak = 5;
+            var expTimeStamp = 0;
+            var expVal = $"R:{expDrops} W:{expNumWins} L:{expNumLosses} Str:{expStreak} Drp:{expDrops} Time:{expTimeStamp}";
+
+            // Act
+            var testClass = new PlayerRating() {
+                Drops = expDrops,
+                NumLosses = expNumLosses,
+                NumWins = expNumWins,
+                Rating = expRating,
+                Streak = expStreak,
+                TimeStamp = expTimeStamp,
+            };
+            var actVal = testClass.ToString();
+
+            // Assert
+            Assert.AreEqual(expVal, actVal);
+        }
+    }
+}
