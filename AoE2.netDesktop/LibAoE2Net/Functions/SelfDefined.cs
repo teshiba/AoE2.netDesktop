b69213b3dd8a027903e8f68643926e3b80c95c96
Satoshi TESHIBA <teshiba.satoshi@gmail.com>
2023-03-18 21:07:51 +0900

    SelfDefined: Add class which define names aoe2.net does NOT define.

---
+++ AoE2.netDesktop/LibAoE2Net/Functions/SelfDefined.cs
@@ -0,0 +1,25 @@
+﻿namespace AoE2NetDesktop.LibAoE2Net.Functions;
+
+using System.Collections.Generic;
+
+using AoE2NetDesktop.LibAoE2Net.JsonFormat;
+
+/// <summary>
+/// Self-defined names, which aoe2.net does NOT define.
+/// </summary>
+public static class SelfDefined
+{
+    /// <summary>
+    /// Gets self-defined apiStrings.
+    /// </summary>
+    public static Strings ApiStrings { get; private set; } = new Strings() {
+        MapType = new List<StringId>() {
+            new StringId() { Id = 175, String = "Morass" },
+        },
+        Civ = new List<StringId> {
+            new StringId() { Id = 40, String = "Dravidians" },
+            new StringId() { Id = 41, String = "Bengalis" },
+            new StringId() { Id = 42, String = "Gurjaras" },
+        },
+    };
+}
